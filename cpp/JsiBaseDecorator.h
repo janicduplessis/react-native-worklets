7645b9d9024e261e49adbb7adb5f57de8e031ee0
Janic Duplessis <janicduplessis@gmail.com>
2023-02-06 13:29:37 -0500

    gitpkg

---
+++ cpp/JsiBaseDecorator.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <jsi/jsi.h>
+
+namespace RNWorklet {
+
+namespace jsi = facebook::jsi;
+
+class JsiBaseDecorator {
+public:
+  virtual void decorateRuntime(jsi::Runtime &runtime) = 0;
+  virtual ~JsiBaseDecorator() {}
+};
+} // namespace RNWorklet
